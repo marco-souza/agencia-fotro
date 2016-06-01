77a477c856fd64003ff19887f9a14d91e060945c
Marco Antônio <ma.souza.junior@gmail.com>
2016-06-01 03:19:06 -0300

    Conexão com MySQL

--- pautas/connect.php
+++ pautas/connect.php
@@ -1,15 +1,14 @@
 <?php
-$link = mysqli_connect("mysql.hostinger.com.br", "u411818826_admin", "PjS3LZwzN9vr", "u411818826_site");
+$servername = "localhost";
+$username = "u411818826_admin";
+$password = "PjS3LZwzN9vr";
 
-if (!$link) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
-
-echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
-
-mysqli_close($link);
+echo "Connected successfully";
 ?>

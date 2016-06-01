512bb1d527b55f0e4349d12cec741e725dec5134
Marco Antônio <ma.souza.junior@gmail.com>
2016-06-01 04:26:58 -0300

    FIX

--- pautas/add-pauta.php
+++ pautas/add-pauta.php
@@ -1,51 +1,47 @@
 <?php
 
 $orientacoes = ($_POST['orientacoes']);
-    // Conexão com BD
-    if( isset($_POST) ) {
+if( isset($_POST) ) {
 
-        // VARS
-        $title = ($_POST['title']);
-        $editoria = ($_POST['editoria']);
-        $data = ($_POST['data']);
-        $cobertura = ($_POST['cobertura']);
-        $licenca = ($_POST['licenca']);
-        $atividade = ($_POST['atividade']);
-        $empresa = ($_POST['empresa']);
-        $endereco = ($_POST['endereco']);
+// VARS
+    $title = ($_POST['title']);
+    $editoria = ($_POST['editoria']);
+    $data = ($_POST['data']);
+    $cobertura = ($_POST['cobertura']);
+    $licenca = ($_POST['licenca']);
+    $atividade = ($_POST['atividade']);
+    $empresa = ($_POST['empresa']);
+    $endereco = ($_POST['endereco']);
 
 // Abrindo Conexão
-        $servername = "localhost";
-        $username = "u411818826_admin";
-        $password = "PjS3LZwzN9vr";
+    $servername = "localhost";
+    $username = "u411818826_admin";
+    $password = "PjS3LZwzN9vr";
+    $dbname = "u411818826_site";
 
-        // Create connection
-        $conn = mysqli_connect($servername, $username, $password);
-        // Check connection
-        if (!$conn) {
-            die("Connection failed: " . mysqli_connect_error());
-        }
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
 
-        // Inserir Pauta na tabela 'pautas'
-        $sql = "INSERT INTO pautas (title, editoria, data, cobertura, licenca, atividade, empresa, endereco, orientacoes)
-        VALUES ('$title', '$editoria', '$data', '$cobertura', '$licenca', '$atividade', '$empresa', '$endereco', '$orientacoes')";
+    // Inserir Pauta na tabela 'pautas'
+    $sql = "INSERT INTO pautas (title, editoria, data, cobertura, licenca, atividade, empresa, endereco, orientacoes)
+    VALUES ('$title', '$editoria', '$data', '$cobertura', '$licenca', '$atividade', '$empresa', '$endereco', '$orientacoes')";
 
-        echo $sql.'<br>';
-
-        if (mysqli_query($conn, $sql)) {
-            // SQL Successfully executed
-            echo "Adicionado com Sucesso!";
-        } else {
-            // Query Error
-            echo "Error executing query ".$sql.": " . mysqli_error($conn);
-        }
+    if (mysqli_query($conn, $sql)) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
 
 // Fechar Conexão
-        mysqli_close($conn);
+    mysqli_close($conn);
 
-    }else{
-        echo ("Existe <b>n&atilde;o<b> POST!");
-    }
+}else{
+    echo ("Existe <b>n&atilde;o<b> POST!");
+}
 
 
 

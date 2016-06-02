d52a4e98a8556c9850ce68cca6f83a9c0ef6a905
Marco Antônio <ma.souza.junior@gmail.com>
2016-06-02 12:45:49 -0300

    Lista de pautas - Formatação da data - FIX

--- pautas/utils.php
+++ pautas/utils.php
@@ -27,7 +27,7 @@ function checkPautas( ){
             // echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
             echo "
             <tr>
-            <td>" . date_format($row["data"], 'd/m/Y às H:i'). "</td>
+            <td>" . date_format(date_create($row["data"]), 'd/m/Y às H:i'). "</td>
             <td>" . $row["title"]. "</td>
             <td>" . $row["editoria"]. "</td>
             <td>" . $row["empresa"]. "</td>
